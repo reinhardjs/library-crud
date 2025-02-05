645e25e821492f68366ee5966584f849484b76ed
reinhardjs <reinhardjsilalahi@gmail.com>
2025-02-05 09:39:15 +0700

    added booksApi api service

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!apiBaseUrl) {
+    throw new Error('API base URL is not defined in the environment variables');
+}
+
+export const API_BASE_URL = apiBaseUrl;
+
+const api = axios.create({
+    baseURL: API_BASE_URL
+})
+
+export const booksApi = {
+    getAll: () => api.get('/books'),
+    create: (data: {
+        title: string;
+        author: string;
+        quantity: number;
+    }) => api.post('/books', data),
+    update: (id: number, data: {
+        title?: string;
+        author?: string;
+        isbn?: string;
+        quantity?: number;
+        categoryId?: number;
+    }) => api.put(`/books/${id}`, data),
+    delete: (id: number) => api.delete(`/books/${id}`)
+};
+
+export default api;
